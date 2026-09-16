9b9c4f3cd51991669198fb62d5326a73012b0c10
Kal-El2-lab <Combsk5@miamioh.edu>
2026-09-16 15:50:28 +0000

    chapter1/dex01 Task1

--- chapter1/dex01/student/sum.py
+++ chapter1/dex01/student/sum.py
@@ -1,4 +1,4 @@
 # Correct the code below.
-first = input("Enter the first integer: ")
-second = input("Enter the second integer: ")
+first = int(input("Enter the first integer: "))
+second = int(input("Enter the second integer: "))
 print("The sum is", first + second)
