528dea8e23c1d565ce3c5312b18bb249416e8a99
Kal-El2-lab <Combsk5@miamioh.edu>
2026-09-18 16:06:44 +0000

    chapter2/dex01 Task1

--- chapter2/dex01/student/salestax.py
+++ chapter2/dex01/student/salestax.py
@@ -5,9 +5,9 @@ Outputs: The purchase price, the tax computed, and the total amount to be paid
 """
 
 purchasePrice = float(input("Enter the purchase price as $: "))
-taxRate = int(input("Enter the tax rate as %: "))
+taxRate = int(input("Enter the tax rate as %: "))/100
 tax = purchasePrice * taxRate
 totalOwed = purchasePrice + tax
 print("Purchase price: ", purchasePrice)
-print("Tax:            ", tax)
-print("Total owed:     ", totalOwed)
+print("Tax:            ", round(tax,2))
+print("Total owed:     ", round(totalOwed,2))
