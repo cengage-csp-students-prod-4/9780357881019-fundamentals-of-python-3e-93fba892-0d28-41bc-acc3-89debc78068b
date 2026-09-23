68bed367057813949082f8abfec2d281f985b0da
Kal-El2-lab <Combsk5@miamioh.edu>
2026-09-23 15:51:12 +0000

    chapter3/ex07 Task1

--- chapter3/ex07/student/salary.py
+++ chapter3/ex07/student/salary.py
@@ -1,7 +1,7 @@
 x= int(input("Enter the starting salary: "))
 y= int(input("Enter the annual percent increase: "))
 z= int(input("Enter the number of years: "))
-print("\nYear   Salary\n-------------")
+print("\nYear   Salary\n----------------")
 for i in range(1,z+1):
     print(format(i, "<7d") + format(x, "6.2f"))
     x = x * (1+y/100)
